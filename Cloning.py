afb7fc68bb75976816aa0cd7cac7994cefafd8ad
Mr.N4w4Nu6 <47638506+Gunawan713@users.noreply.github.com>
2019-03-13 09:26:25 +0700

    Add files via upload

---
+++ Cloning.py
@@ -0,0 +1,130 @@
+#!/data/data/com.termux/files/usr/bin/bash
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+date |lolcat
+cowsay -f eyes ["Tols B.S.T"]|lolcat
+echo "<<============>++<=============>>"|lolcat
+echo "Tols : BL4CKSH4D0W TE4M"|lolcat
+echo "Created by : Mr.N4w4Nu6"|lolcat
+echo "Contact : nizarija65@gmail.com"|lolcat
+echo "<<============>++<=============>>"|lolcat
+###################################################
+# CTRL C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  "(Ctrl + C ) Detected, Trying To Exit... "|lolcat
+sleep 1
+echo ""
+echo "Thank for Coly ing ... "|lolcat
+sleep 1
+echo ""
+echo "#Coly in here  ... "|lolcat
+exit
+}
+echo ""
+sleep 1
+     echo "\033[34;1m SPAM SMS Yang Tersedia "
+     echo "   '(01)'Spam Mr.N4w4Nu6 "
+     echo "   '(02)'BL4CKSH4D0W TE4M "
+     echo "   '(03)'Artis Bokep.      "
+     echo "   '(04)'Vanessa Angel"
+     echo "   '(05)'Nikita Wily "
+     echo "   '(06)'Bencong Kolong"
+sleep 1
+echo 
+     echo "    (07)INFO "
+     echo "    (08)Install Bahan"
+     echo "    (09)Keluar "
+echo " pilih nomer nya "
+read ezz
+if [ $ezz = 1 ] || [ $ezz = 01 ]
+then
+clear
+echo "\033[31;1m"
+toilet -f mono12 -F gay "Mr.N4w4Nu6"
+php jdid.php
+fi
+
+if [ $ezz = 02 ] || [ $ezz = 2 ]
+then
+clear
+echo "\033[36;1m"
+toilet -f  mono12 -F gay "BL4CKSH4D0W TE4M"
+php telkomsel.php
+fi
+
+if [ $ezz = 03 ] || [ $ezz = 3 ]
+then
+clear 
+echo  "\033[39;1m"
+toilet -f mono12 -F metal "Artis Bokep"
+php phd.php
+fi
+
+if [ $ezz = 04 ] || [ $ezz = 4 ]
+then
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "Vanessa Angel"
+php mataharimall.php
+fi
+
+if [$ezz = 05 ] || [ $ezz = 5 ]
+then
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "Nikita Wily"
+python2 spammer.py
+fi
+
+if [ $ezz = 06 ] || [ $ezz = 6 ]
+then
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "Bencong Kolong"
+php tokped.php
+fi
+
+if [ $ezz = 07 ] || [ $ezz = 7 ]
+then 
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "INFO"
+echo "Dibuat oleh "
+echo "Mr.N4w4Nu6 | BST"
+echo "versi 0.1"
+echo "THANKS TO ALL MEMBER BL4CKSH4D0W TE4M"
+echo ""
+fi
+
+
+if [ $ezz = 08 ] || [ $ezz =8 ]
+then
+clear
+echo "Loading ----------------------------------"|lolcat
+sh install.sh
+sh gco.sh
+fi
+
+if [ $ezz = 09 ] || [ $ezz =9 ]
+then
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "KELUAR NGENTOD"
+echo "semoga hari mu menyenangkan"
+fi
